d214004d10b0f726fb1e25a6e16ba19b8950863a
JordanWitt <wittjordan94@gmail.com>
2023-03-03 14:48:06 -0600

    more notes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,7 @@
-package org.example;
+package org.example;//always terminate statement with semicolon
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) { //void means it will not return a value
+        System.out.println("Hello world!"); //where code is being written
     }
 }
