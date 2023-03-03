cf453880afdcc89cf8cee0de981fc647feab5e5f
JordanWitt <wittjordan94@gmail.com>
2023-03-03 16:43:50 -0600

    finals/consts

---
+++ src/main/java/org/example/Constants.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Constants {
+    public static void main(String[] args) {
+        final float PI = 3.14F; //final = const
+        //const should be named in ALL CAPS
+        System.out.println(PI);
+    }
+}
