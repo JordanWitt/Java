1d96eee4aef66fa23091c9dc7c91682f1ae4bdf9
JordanWitt <wittjordan94@gmail.com>
2023-03-03 16:52:22 -0600

    arithetic expression

---
+++ src/main/java/org/example/ArithmeticExpressions.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class ArithmeticExpressions {
+    public static void main(String[] args) {
+        int result = 10 + 3;
+        System.out.println(result);
+        result = 10 / 3;
+        System.out.println(result);
+        double result2 = (double)10 / (double)3;
+        //prints a double result
+        System.out.println(result2);
+        int x = 1;
+        x += 2; //+= augmented assignment operator/ or compound
+        System.out.println(x);
+    }
+}
