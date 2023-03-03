48293560d3d58153f0af566dc62eaba4049b13cd
JordanWitt <wittjordan94@gmail.com>
2023-03-03 16:19:04 -0600

    arrays

--- src/main/java/org/example/Strings.java
+++ src/main/java/org/example/Strings.java
@@ -3,7 +3,7 @@ package org.example;
 public class Strings {
     public static void main(String[] args) {
         String myName = "Jordan Witt" + "!"; //string literal
-        System.out.println(myName.endsWith("!"));
+        System.out.println(myName.endsWith("!"));//returns true
         System.out.println(myName.length());
         System.out.println(myName.indexOf("o" + "e")); //logs -1 because e doesn't exist
         System.out.println(myName.replace("J", "B"));
@@ -16,5 +16,9 @@ public class Strings {
         System.out.println(myName.trim());
         System.out.println(myName.toLowerCase()); //converts all lower case
         System.out.println(myName.toUpperCase()); //all upper case
+
+        //include special characters
+        String message = "Hello \"Jordan\"";
+        System.out.println(message); //escaping characters to add quotes
     }
 }
