b83d17ccf74c5055e5b3dcfa103f29a5076cbbb7
JordanWitt <wittjordan94@gmail.com>
2023-03-03 16:40:51 -0600

    java arrays and matrix

--- src/main/java/org/example/MultiDemensionalArrays.java
+++ src/main/java/org/example/MultiDemensionalArrays.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class MultiDemensionalArrays {
     public static void main(String[] args) {
-
+        int[][] numbers = { { 1, 2, 3 }, { 4,5,6 }, { 7,8,9 } };
+        System.out.println(Arrays.deepToString(numbers));
+        //deepToString used for printing multi=demensional arrays
     }
 }
