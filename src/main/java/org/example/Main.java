682c94498365a2165ee9a112afb7a28262900e7d
JordanWitt <wittjordan94@gmail.com>
2023-03-03 15:14:21 -0600

    notes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;//always terminate statement with semicolon
 
 public class Main {
     public static void main(String[] args) { //void means it will not return a value
+        int age = 30; //int type of variable and age is the identifier 30 is the value
         System.out.println("Hello world!"); //where code is being written
+        System.out.println(age);
     }
 }
