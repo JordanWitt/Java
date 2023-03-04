9aa442af9cbcaf8963020914ba17bae896f51c75
JordanWitt <wittjordan94@gmail.com>
2023-03-04 16:46:48 -0600

    math class

---
+++ src/main/java/org/example/Casting.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Casting {
+    public static void main(String[] args) {
+        double x = 1;
+        int y = (int)x + 2; //casting int
+        //implicit casting
+        //byte can be converted to short and short to int and int to long and float to double
+        System.out.println(y);
+    }
+}
---
+++ src/main/java/org/example/MathClass.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class MathClass {
+    public static void main(String[] args) {
+
+    }
+}
