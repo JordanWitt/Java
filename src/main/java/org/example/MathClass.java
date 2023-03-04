839b00e31d40ee809f6479c4f19c3c70897ceb1b
JordanWitt <wittjordan94@gmail.com>
2023-03-04 16:55:16 -0600

    basic math methods

--- src/main/java/org/example/MathClass.java
+++ src/main/java/org/example/MathClass.java
@@ -2,6 +2,13 @@ package org.example;
 
 public class MathClass {
     public static void main(String[] args) {
-
+        int result =  Math.round(1.1F); //rounds to the nearest whole
+        System.out.println(result);
+        double result1 = Math.ceil(123.2);//anything over the whole is rounded to next highest int
+        System.out.println(result1);
+        int result2 = (int) Math.floor(234.9); // drops the decimal to a whole
+        System.out.println(result2);
+        int result3 = (int) (Math.random() * 50);
+        System.out.println(result3);
     }
 }
