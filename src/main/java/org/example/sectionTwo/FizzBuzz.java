4064aa34392d56d545c3d936d1ccc7eafc1de5ba
JordanWitt <wittjordan94@gmail.com>
2023-03-06 14:38:09 -0600

    fizzbuzz

---
+++ src/main/java/org/example/sectionTwo/FizzBuzz.java
@@ -0,0 +1,19 @@
+package org.example.sectionTwo;
+
+import java.util.Scanner;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Number: ");
+        int number = scanner.nextInt();
+        if(number % 5 == 0 && number % 3 == 0) //most complex needs to be executed first and generic below to ensure it gets ran,
+            System.out.println("FizzBuzz");
+        else if (number % 3 == 0 )
+            System.out.println("Buzz");
+        else if (number % 5 == 0)
+            System.out.println("Fizz");
+        else
+            System.out.println("Can't compile " + number);
+    }
+}
