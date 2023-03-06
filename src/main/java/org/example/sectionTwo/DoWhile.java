a86f7675aad673a4c212c009d73b7caae7e45a61
JordanWitt <wittjordan94@gmail.com>
2023-03-06 15:03:18 -0600

    do while

---
+++ src/main/java/org/example/sectionTwo/DoWhile.java
@@ -0,0 +1,15 @@
+package org.example.sectionTwo;
+
+import java.util.Scanner;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        String input = "";
+        do{
+            System.out.println("Input: ");
+            Scanner scanner = new Scanner(System.in);
+            input = scanner.next().toLowerCase();
+        }while (!input.equals("quit"));
+    }// do while loops will get executed at least once  even if the condition is false
+}
+
