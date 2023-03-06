61bc7377047de2392c4e657012c6b016c8a97721
JordanWitt <wittjordan94@gmail.com>
2023-03-06 14:05:13 -0600

    clean ternary operator

---
+++ src/main/java/org/example/sectionTwo/TernaryOperator.java
@@ -0,0 +1,9 @@
+package org.example.sectionTwo;
+
+public class TernaryOperator {
+    public static void main(String[] args) {
+        int income = 120_000;
+        String className = income > 100_000 ? "First" : "Economy"; //colon ternary operator
+        System.out.println(className);
+    }
+}
