eb291170d020c877c2f9b6fa3316fcc2875d2464
JordanWitt <wittjordan94@gmail.com>
2023-04-16 18:52:14 -0500

    saving

---
+++ src/main/java/org/example/sectionFour/ConsoleIO.java
@@ -0,0 +1,10 @@
+package org.example.sectionFour;
+
+public class ConsoleIO {
+    public static void main(String[] args) {
+        String name = "Jordan";
+        System.out.printf("Hello there %s. Nice to see you.%n How is your day?", name); //%n is like tab new line
+    }
+
+
+}
