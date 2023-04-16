ac0b26c7b30b7b2e35a3b782a632636ecd9c2a30
JordanWitt <wittjordan94@gmail.com>
2023-04-16 18:17:47 -0500

    section two codeup java

---
+++ src/main/java/org/example/sectionFour/JavaTwo.java
@@ -0,0 +1,11 @@
+package org.example.sectionFour;
+
+public class JavaTwo {
+    public static void main(String[] args) {
+        {
+            System.out.println("Code inside of curly braces should be indented");
+            System.out.println("with four spaces");
+        }
+
+    }
+}
--- src/main/java/org/example/sectionFour/javaOne.java
+++ src/main/java/org/example/sectionFour/javaOne.java
@@ -2,5 +2,7 @@ package org.example.sectionFour;
 
 public class javaOne {
   //OBJECT ORIENTED LANGUAGE
-
+  public static void main(String[] args) {
+      System.out.println("Hello, Jordan");
+  }
 }
