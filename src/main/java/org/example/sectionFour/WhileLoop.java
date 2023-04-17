56d7017e5a9518a7790ba352b72fdc8a1502f0e9
JordanWitt <wittjordan94@gmail.com>
2023-04-17 13:54:43 -0500

    do while, and while loops

---
+++ src/main/java/org/example/sectionFour/WhileLoop.java
@@ -0,0 +1,16 @@
+package org.example.sectionFour;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int i = 5;
+        while (i <= 15){
+            System.out.println(i++);
+        }
+        System.out.println("----------NEXT------------");
+        int num = 0;
+        do{
+            System.out.println(num);
+            num += 2;
+        }while (num <=100);
+    }
+}
