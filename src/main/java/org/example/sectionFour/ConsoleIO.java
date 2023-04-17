54af2d173940d39288679fdcb3dcd0c8b6982b9a
JordanWitt <wittjordan94@gmail.com>
2023-04-16 19:23:53 -0500

    commit

--- src/main/java/org/example/sectionFour/ConsoleIO.java
+++ src/main/java/org/example/sectionFour/ConsoleIO.java
@@ -4,7 +4,9 @@ public class ConsoleIO {
     public static void main(String[] args) {
         String name = "Jordan";
         System.out.printf("Hello there %s. Nice to see you.%n How is your day?", name); //%n is like tab new line
+
+        String greeting = "Salutations";
+        System.out.printf("%n %s, %s!", greeting, name);
+
     }
-
-
 }
