8097f4d0aefb8abe908995e3d1fd53db101a1a98
JordanWitt <wittjordan94@gmail.com>
2023-04-17 11:05:32 -0500

    scanner exercise

--- src/main/java/org/example/sectionFour/ScannerExample.java
+++ src/main/java/org/example/sectionFour/ScannerExample.java
@@ -1,4 +1,5 @@
 package org.example.sectionFour;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class ScannerExample {
@@ -10,5 +11,19 @@ public class ScannerExample {
 
         System.out.println("You entered: --> \"" + userInput + "\" <--");
 
+
+        String password = "password";
+        System.out.println("Type in the password");
+        String userGuess = scanner.nextLine(); // Use nextLine() to read the entire line
+        while (true) {
+            if (userGuess.equals(password)) {
+                System.out.println("You've guessed correctly");
+                break; // Exit the loop if the password is correct
+            } else {
+                System.out.println("Try again!");
+                userGuess = scanner.nextLine(); // Read user input again
+            }
+        }
+        scanner.close(); // Close the scanner after use
     }
 }
