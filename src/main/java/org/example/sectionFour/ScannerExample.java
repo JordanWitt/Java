87793f2bcc0c96bdedbda0d17b4efe1419852a16
JordanWitt <wittjordan94@gmail.com>
2023-04-17 10:49:50 -0500

    scanner

---
+++ src/main/java/org/example/sectionFour/ScannerExample.java
@@ -0,0 +1,14 @@
+package org.example.sectionFour;
+import java.util.Scanner;
+
+public class ScannerExample {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter something: ");
+        String userInput = scanner.next();
+
+        System.out.println("You entered: --> \"" + userInput + "\" <--");
+
+    }
+}
