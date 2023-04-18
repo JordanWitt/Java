1d4e1df6a912d787dae800b62c52ced2d24677d8
JordanWitt <wittjordan94@gmail.com>
2023-04-18 16:02:23 -0500

    do while

--- src/main/java/org/example/sectionFour/WhileLoop.java
+++ src/main/java/org/example/sectionFour/WhileLoop.java
@@ -19,5 +19,23 @@ public class WhileLoop {
             System.out.println(nums);
             nums -= 5;
         } while (nums >= -10);
+
+        System.out.println("----------NEXT------------");
+        for(int j = 1; j <= 10; j++) { // <--+
+            if(j % 2 == 1) { //              |
+                continue; // ----------------+
+            }
+            System.out.println(j);
+        }
+
+        System.out.println("----------NEXT------------");
+        int n = 2; // starting number
+        int limit = 1000000; // limit for number
+
+        do {
+            int square = n * n; // calculate square
+            System.out.println(square); // display square
+            n = square; // update num with square value
+        } while (n > 0 && n < limit); // repeat until num is less than limit
     }
 }
