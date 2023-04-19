4ad4f9cc7bf61bf4ffe51e0d71a7b051c525d272
JordanWitt <wittjordan94@gmail.com>
2023-04-19 12:45:44 -0500

    saving

--- src/main/java/org/example/sectionFour/BasicArithmetic.java
+++ src/main/java/org/example/sectionFour/BasicArithmetic.java
@@ -5,9 +5,10 @@ public class BasicArithmetic {
         return         (x + y) +
                 "\n" + (x * y) +
                 "\n" + (x / y) +
-                "\n" + (x - y);
+                "\n" + (x - y) +
+                "\n" + (x % y); //modulus, leave the remainder of the number left, divides and prints what's left over
     }
     public static void main(String[] args) {
-        System.out.println(arithmeticExpression(10, 5));
+        System.out.println(arithmeticExpression(11, 5));
     }
 }
