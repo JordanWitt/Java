45b610a0d2fface45d194e189fdca487a0a9dbd1
JordanWitt <wittjordan94@gmail.com>
2023-04-19 09:54:11 -0500

    Bob Console exercise

---
+++ src/main/java/org/example/sectionFour/Bob.java
@@ -0,0 +1,35 @@
+package org.example.sectionFour;
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        for(;;) {
+            String userResponse;
+            String casualResponse = "sure";
+            String aggressiveResponse = "Whoa, chill out";
+            String fineResponse = "Fine, be that way";
+            String psshhResponse = "Whatever";
+
+            System.out.println("Hey-yo, My name is Bob, ask me anything");
+            userResponse = scanner.nextLine();
+            System.out.println("You entered:");
+            System.out.println(userResponse);
+
+            if (userResponse.endsWith("?")) {
+                System.out.println(casualResponse);
+            } else if (userResponse.endsWith("!")) {
+                System.out.println(aggressiveResponse);
+            } else if (userResponse.isEmpty()) {
+                System.out.println(fineResponse);
+            } else
+                System.out.println(psshhResponse);
+            System.out.println("Would you like to go again? (Y/N)");
+            userResponse = scanner.nextLine();
+            if(!userResponse.equalsIgnoreCase("Y")){
+                break;
+            }
+        }
+        scanner.close();
+    }
+}
