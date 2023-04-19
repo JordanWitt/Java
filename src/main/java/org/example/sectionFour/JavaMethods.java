67f3878f07f58fb9ad51eac5d81911c005c1ab1b
JordanWitt <wittjordan94@gmail.com>
2023-04-19 11:58:24 -0500

    more simple methods

--- src/main/java/org/example/sectionFour/JavaMethods.java
+++ src/main/java/org/example/sectionFour/JavaMethods.java
@@ -4,7 +4,16 @@ public class JavaMethods {
     public static String sayHello(String name){
         return String.format("Hello, %s!", name);
     }
+    public static int returnThree() {
+        return 3;
+    }
+    public static String shout(String s) {
+        return s.toUpperCase() + "!!!";
+    }
+
     public static void main(String[] args) {
         System.out.println(sayHello("Jordan"));
+        System.out.println(returnThree());
+        System.out.println(shout("ahhhhh"));
     }
 }
