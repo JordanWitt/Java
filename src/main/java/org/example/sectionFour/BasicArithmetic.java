6e12533ec9ff630801283ed06ce3b3f9b7005f74
JordanWitt <wittjordan94@gmail.com>
2023-04-19 12:56:51 -0500

    recursion

--- src/main/java/org/example/sectionFour/BasicArithmetic.java
+++ src/main/java/org/example/sectionFour/BasicArithmetic.java
@@ -8,7 +8,16 @@ public class BasicArithmetic {
                 "\n" + (x - y) +
                 "\n" + (x % y); //modulus, leave the remainder of the number left, divides and prints what's left over
     }
+    public static void recursionMethod(int x, int y){
+        if(x * y <= 0){
+            System.out.println("Finished");
+            return;
+        }
+        System.out.println(x + y);
+        recursionMethod(x,y - 1);
+    }
     public static void main(String[] args) {
         System.out.println(arithmeticExpression(11, 5));
+        recursionMethod(12, 10);
     }
 }
