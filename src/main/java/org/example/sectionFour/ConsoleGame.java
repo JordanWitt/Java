b33172ef237b7689b3516d1f13a3693269bfcc0f
JordanWitt <wittjordan94@gmail.com>
2023-04-20 12:12:51 -0500

    working on console game

---
+++ src/main/java/org/example/sectionFour/ConsoleGame.java
@@ -0,0 +1,11 @@
+package org.example.sectionFour;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class ConsoleGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+    }
+}
