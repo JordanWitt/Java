2cbe8e428f6d2457392fd2f9dd08619d32e6a0db
JordanWitt <wittjordan94@gmail.com>
2023-04-20 13:17:41 -0500

    working on player damage and stats

--- src/main/java/org/example/sectionFour/ConsoleGame.java
+++ src/main/java/org/example/sectionFour/ConsoleGame.java
@@ -1,5 +1,6 @@
 package org.example.sectionFour;
 
+import java.sql.SQLOutput;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -9,15 +10,60 @@ public class ConsoleGame {
         Random random = new Random();
         //player health
         int player = 100;
+        int playerDamage = 30;
         //enemy base stats
         String[] enemies = {"Draven", "Sion", "Kled", "Riven"};
-        int enemyMaxHealth = 88;
+        int enemyMaxHealth = 75;
         int enemyAttackDamage = 25;
+
+        //turret
+        int turret = 50;
 
         //base items/mechanics
         int recall = 50; //restores base health
         int item = 10; //adds 10 dmg
         int critChance = 5; //critical chance every 3 hits
+        int armour = 5; //armour for the tank champs
+        int lifeSteal = 5; //life-steal
 
+        boolean running = true;
+        boolean minionsSpawned = false;
+
+        System.out.println("-----WELCOME TO SUMMONERS RIFT-----");
+        GAME:
+        while (running) {
+            if (!minionsSpawned) {
+                try {
+                    System.out.println("Minions will appear in 5 seconds");
+                    Thread.sleep(5000);
+                    System.out.println("Minions arrived to lane");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                minionsSpawned = true;
+            }
+            String enemy = enemies[random.nextInt(enemies.length)]; // random enemy on kill
+            System.out.println("\t# " + enemy + " arrived to lane #\n");
+
+            while (enemyMaxHealth > 0) { // iteration of attacks/player options
+                System.out.println("\tYour HP: " + player); //player health
+                if(enemy.equals("Sion")||enemy.equals("Riven")){
+                    enemyMaxHealth += armour;
+                } else if (enemy.equals("Draven")|| enemy.equals("Kled")) {
+                    enemyAttackDamage += lifeSteal;
+                }
+                System.out.println("\t" + enemy + "'s HP: " + enemyMaxHealth); //e health
+                System.out.println("\n\tWhat would you like to do?");
+                System.out.println("\t1 Attack");
+                System.out.println("\t2 recall");
+                System.out.println("\t3 Surrender!\n"); //player options
+
+                String input = scanner.nextLine();
+                if(input.equals("1")){
+                    enemyMaxHealth -= enemyAttackDamage;
+                    player -= enemyAttackDamage; //health goes down
+                }
+            }
+        }
     }
 }
