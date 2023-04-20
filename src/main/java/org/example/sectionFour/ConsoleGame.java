120e8e32bc90ef55e1e5beb65ed94b65c356ce81
JordanWitt <wittjordan94@gmail.com>
2023-04-20 12:22:12 -0500

    working on players

--- src/main/java/org/example/sectionFour/ConsoleGame.java
+++ src/main/java/org/example/sectionFour/ConsoleGame.java
@@ -7,5 +7,17 @@ public class ConsoleGame {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
+        //player health
+        int player = 100;
+        //enemy base stats
+        String[] enemies = {"Draven", "Sion", "Kled", "Riven"};
+        int enemyMaxHealth = 88;
+        int enemyAttackDamage = 25;
+
+        //base items/mechanics
+        int recall = 50; //restores base health
+        int item = 10; //adds 10 dmg
+        int critChance = 5; //critical chance every 3 hits
+
     }
 }
