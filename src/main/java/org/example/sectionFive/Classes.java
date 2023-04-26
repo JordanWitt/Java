76de8947e4cc4b9980a76e7379fa64bc85c042c5
JordanWitt <wittjordan94@gmail.com>
2023-04-26 11:33:19 -0500

    working on classes and objects, creating grade checker

---
+++ src/main/java/org/example/sectionFive/Classes.java
@@ -0,0 +1,26 @@
+package org.example.sectionFive;
+
+public class Classes {
+
+    static class Student{
+    public double grade;
+    public String name;
+    boolean isStudent;
+
+    public Student(String studentName, double studentGrade, boolean isStudent){
+        this.grade = studentGrade;
+        this.name = studentName;
+        this.isStudent = isStudent;
+    }
+    public int gradeAverageCheck(){
+        if(grade == 90 || grade == 100){
+            System.out.println("You received and A");
+        }else if
+    }
+
+        public static void main(String[] args) {
+
+        }
+
+    }
+}
