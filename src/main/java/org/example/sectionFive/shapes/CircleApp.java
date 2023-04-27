03e0997742be059ea0ac04c44d9451457191bcb1
JordanWitt <wittjordan94@gmail.com>
2023-04-27 15:04:29 -0500

    circle app

---
+++ src/main/java/org/example/sectionFive/shapes/CircleApp.java
@@ -0,0 +1,16 @@
+package org.example.sectionFive.shapes;
+import org.example.sectionFive.util.Input;
+
+
+public class CircleApp {
+    public static void main(String[] args) {
+        Input input = new Input();
+        int attempt = 0;
+        int runthroughs = 5;
+        boolean running = true;
+        boolean goAgain = input.yesNo();
+
+        System.out.println("\t\t\t\t\tLet's make a circle");
+        System.out.println();
+    }
+}
