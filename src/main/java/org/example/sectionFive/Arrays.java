6dfd849bc1bbf91fb166e42ecec7d212a6eb06a6
JordanWitt <wittjordan94@gmail.com>
2023-05-10 13:13:34 -0500

    getters and setters

--- src/main/java/org/example/sectionFive/Arrays.java
+++ src/main/java/org/example/sectionFive/Arrays.java
@@ -1,10 +1,26 @@
 package org.example.sectionFive;
 
 public class Arrays {
+
     public static void main(String[] args) {
-        double[] prices;
-        prices = new double[4];
-        System.out.println(java.util.Arrays.toString(prices));
+
+        final int NUMBER_OF_BEATLES = 4;
+        BandMember[] theBeatles = new BandMember[NUMBER_OF_BEATLES];
+
+        theBeatles[0] = new BandMember();
+        theBeatles[0].setMembers(new String[] { "John Lennon", "Guitar" });
+
+        theBeatles[1] = new BandMember();
+        theBeatles[1].setMembers(new String[] { "Paul McCartney", "Bass" });
+
+        theBeatles[2] = new BandMember();
+        theBeatles[2].setMembers(new String[] { "George Harrison", "Guitar" });
+
+        theBeatles[3] = new BandMember();
+        theBeatles[3].setMembers(new String[] { "Ringo Starr", "Drums" });
+
+        System.out.println(java.util.Arrays.toString(theBeatles));
+
 
     }
 }
---
+++ src/main/java/org/example/sectionFive/BandMember.java
@@ -0,0 +1,13 @@
+package org.example.sectionFive;
+
+public class BandMember {
+    String[] members;
+
+    public String[] getMembers() {
+        return members;
+    }
+
+    public void setMembers(String[] members) {
+        this.members = members;
+    }
+}
