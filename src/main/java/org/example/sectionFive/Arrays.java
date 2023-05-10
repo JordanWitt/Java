7e18756a4849bb4380cf30e72d7a992194b7b2d3
JordanWitt <wittjordan94@gmail.com>
2023-05-10 12:26:14 -0500

    arrays

---
+++ src/main/java/org/example/sectionFive/Arrays.java
@@ -0,0 +1,10 @@
+package org.example.sectionFive;
+
+public class Arrays {
+    public static void main(String[] args) {
+        double[] prices;
+        prices = new double[4];
+        System.out.println(java.util.Arrays.toString(prices));
+
+    }
+}
