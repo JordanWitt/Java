e39421a5f58daff262b9210dca76cbc2d6c60008
JordanWitt <wittjordan94@gmail.com>
2023-05-11 14:03:05 -0500

    allowed user to store new movie

--- src/main/java/org/example/sectionFive/movies/MovieApplication.java
+++ src/main/java/org/example/sectionFive/movies/MovieApplication.java
@@ -1,14 +1,12 @@
 package org.example.sectionFive.movies;
-
-import org.example.sectionFive.util.Input;
-
+import java.util.Scanner;
 import java.util.Arrays;
 
 public class MovieApplication {
     public static void main(String[] args) {
         int attempt = 0;
-        Input movie = new Input();
-        Movie[] movieList = Arrays.copyOf(MoviesArray.findAll(), 100);
+        Scanner scanner = new Scanner(System.in);
+        Movie[] movieList = Arrays.copyOf(MoviesArray.findAll(), 200);
 
         System.out.println("-----------------------MOVIES-----------------------");
         System.out.println();
@@ -23,8 +21,9 @@ public class MovieApplication {
             System.out.println("\t5 - View movies in sci-fi category");
             System.out.println("\t6 - View movies in musical category");
             System.out.println("\t7 - View movies in comedy category");
+            System.out.println("\t8 - Add a new movie");
 
-            int decision = movie.getInt();
+            int decision = scanner.nextInt();
             attempt++;
 
             if (decision == 0) {
@@ -33,19 +32,24 @@ public class MovieApplication {
                 viewAllMovies(movieList);
             } else if (decision >= 2 && decision <= 7) {
                 viewMoviesByCategory(movieList, decision);
+            } else if (decision == 8) {
+                addNewMovie(movieList, scanner);
             } else {
                 System.out.println("Invalid input. Please try again.");
             }
         }
 
         System.out.println("See you later! Here's how many times you viewed our Movies: " + attempt);
+        scanner.close();
     }
 
     private static void viewAllMovies(Movie[] movies) {
         System.out.println("All movies: ");
         for (Movie movie : movies) {
-            System.out.println(movie.getName());
-            System.out.println("--------------------");
+            if (movie != null) {
+                System.out.println(movie.getName());
+                System.out.println("--------------------");
+            }
         }
     }
 
@@ -53,11 +57,41 @@ public class MovieApplication {
         String categoryName = getCategoryName(category);
         System.out.println(categoryName + " movies: ");
         for (Movie movie : movies) {
-            if (movie.getCategory().equals(categoryName.toLowerCase())) {
+            if (movie != null && movie.getCategory().equalsIgnoreCase(categoryName)) {
                 System.out.println(movie.getName());
                 System.out.println("--------------------");
             }
         }
+    }
+
+    private static void addNewMovie(Movie[] movies, Scanner scanner) {
+        scanner.nextLine(); // Consume the newline character
+
+        System.out.println("Enter the name of the movie:");
+        String name = scanner.nextLine();
+
+        System.out.println("Enter the category of the movie:");
+        String category = scanner.nextLine();
+
+        Movie newMovie = new Movie(name, category);
+
+        // Find the first available slot in the movie array
+        int index = -1;
+        for (int i = 0; i < movies.length; i++) {
+            if (movies[i] == null) {
+                index = i;
+                break;
+            }
+        }
+
+        if (index != -1) {
+            movies[index] = newMovie;
+            System.out.println("Movie added successfully!");
+        } else {
+            System.out.println("Movie list is full. Unable to add a new movie.");
+        }
+
+        System.out.println("--------------------");
     }
 
     private static String getCategoryName(int category) {
